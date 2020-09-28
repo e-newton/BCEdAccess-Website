fd7ef420f9852c9c09980244c8c7a658f7d00b63
e-newton <ericnewton2251@gmail.com>
2020-09-28 10:35:56 -0700

    blog-root routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BlogRootComponent} from './components/blog-root/blog-root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blogs', component: BlogRootComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
