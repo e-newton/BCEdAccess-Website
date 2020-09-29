8d4b4da20cc21ce762432185c4ebaa5036ce365b
e-newton <ericnewton2251@gmail.com>
2020-09-28 21:25:01 -0700

    Added colour test

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -2,6 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
+function hexToRgb(hex): any {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
@@ -26,4 +35,12 @@ describe('HeaderComponent', () => {
     const img = fixture.debugElement.nativeElement.querySelector('#logo');
     expect(img.src).toContain('bcedaccess_logo.png');
   });
+  it('should have the correct style', () => {
+    const nav = fixture.debugElement.nativeElement.querySelector('nav');
+    const rgb = hexToRgb(component.style['background-color']);
+    const col = 'background-color: rgb(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ')';
+
+    expect(nav.attributes.style.value).toContain(col);
+  });
+
 });
