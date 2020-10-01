8b28018e981fe099da3dc2bc9807dafcc2c16a9f
e-newton <ericnewton2251@gmail.com>
2020-10-01 10:37:20 -0700

    Heroku Setup

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const process = require('process')
+require('dotenv').config()
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if(process.env.PRODUCTION){
+  app.use(express.static(__dirname + '/dist/BCEdAccess-Website'));
+
+  app.get('/*', function(req,res) {
+
+    res.sendFile(path.join(__dirname+'/dist/BCEdAccess-Website/index.html'));
+  });
+} else{
+  app.use(express.static('./src/'))
+  app.get('./*', function(req,res) {
+    res.sendFile(path.join('src/index.html'));
+  });
+
+}
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
