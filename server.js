5d40132b5eaa4ec303b37031827c4879339dc21d
e-newton <ericnewton2251@gmail.com>
2020-10-04 20:56:03 -0700

    Database setup (I think I did this on the wrong branch...)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const process = require('process')
+const { Client } = require('pg');
+
 require('dotenv').config()
 
 const app = express();
@@ -14,6 +16,7 @@ app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,6 +40,20 @@ if(process.env.PRODUCTION){
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
+
+
+const client = new Client();
+client.connect( err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected to database')
+  }
+});
+
+
+
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
