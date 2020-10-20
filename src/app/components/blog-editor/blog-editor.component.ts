1ec23f9b717f3fc5ed0d661b275941768bf5e7d9
e-newton <ericnewton2251@gmail.com>
2020-10-20 16:34:01 -0700

    Added create blog

--- src/app/components/blog-editor/blog-editor.component.ts
+++ src/app/components/blog-editor/blog-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BlogService} from '../../services/blog.service';
 import {NgForm} from '@angular/forms';
+import {Blog} from '../../model/blog';
 
 @Component({
   selector: 'app-blog-editor',
@@ -27,5 +28,12 @@ export class BlogEditorComponent implements OnInit {
 
   onSubmit(f: NgForm): void {
       console.log(f.value);
+      this.title = f.value.title;
+      this.author = f.value.author;
+      this.body = f.value.author;
+  }
+
+  createBlog(): Blog {
+    return new Blog(this.id, this.title, this.author, this.body);
   }
 }
