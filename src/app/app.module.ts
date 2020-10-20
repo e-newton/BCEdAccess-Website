9bbdf964a111e25e267a409e8fcbbec176c02b4d
e-newton <ericnewton2251@gmail.com>
2020-10-20 16:12:10 -0700

    Created blog-editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BlogRootComponent } from './components/blog-root/blog-root.component';
 import { BlogPreviewComponent } from './components/blog-preview/blog-preview.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BlogViewComponent } from './components/blog-view/blog-view.component';
+import { BlogEditorComponent } from './components/blog-editor/blog-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogRootComponent,
     BlogPreviewComponent,
-    BlogViewComponent
+    BlogViewComponent,
+    BlogEditorComponent
   ],
   imports: [
     BrowserModule,
