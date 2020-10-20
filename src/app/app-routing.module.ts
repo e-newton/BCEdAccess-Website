57d368e527fad91f45e9776f1489a60b7fe6c8cb
e-newton <ericnewton2251@gmail.com>
2020-10-20 16:14:12 -0700

    Created blog/editor route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BlogRootComponent} from './components/blog-root/blog-root.component';
 import {BlogViewComponent} from './components/blog-view/blog-view.component';
+import {BlogEditorComponent} from './components/blog-editor/blog-editor.component';
 
 const routes: Routes = [
-
+  {path: 'blog/editor', component: BlogEditorComponent},
   {path: 'blog/:id', component: BlogViewComponent},
   {path: 'blog', component: BlogRootComponent},
 
