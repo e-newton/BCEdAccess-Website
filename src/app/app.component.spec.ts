fc9b7887c846fc3f491fe6f5a4d7354aafd4c45b
e-newton <ericnewton2251@gmail.com>
2020-10-29 17:49:08 -0700

    Made a quick change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,6 +21,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    console.log('Wow a change!');
     expect(app).toBeTruthy();
   });
 
