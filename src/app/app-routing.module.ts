088ccf6221fb87e3915a3abdceca91c9c135642a
e-newton <ericnewton2251@gmail.com>
2020-11-02 10:17:09 -0800

    Added edit route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {BlogViewComponent} from './components/blog-view/blog-view.component';
 import {BlogEditorComponent} from './components/blog-editor/blog-editor.component';
 
 export const routes: Routes = [
+  {path: 'blog/editor/:id', component: BlogEditorComponent},
   {path: 'blog/editor', component: BlogEditorComponent},
   {path: 'blog/:id', component: BlogViewComponent},
   {path: 'blog', component: BlogRootComponent},
