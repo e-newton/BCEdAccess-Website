c6ef71477594775ffbcce07fe5ba3e071100afec
e-newton <ericnewton2251@gmail.com>
2020-11-05 19:15:17 -0800

    Initialization of firebase service and did some sort of compatibility thing with angular.json ng build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,22 @@ import { BlogEditorComponent } from './components/blog-editor/blog-editor.compon
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+
+export const environment = {
+  production : false,
+  firebase : {
+    apiKey: 'AIzaSyDk07G7MeFlY_A5AyK2e47jWQePJSbyI3o',
+    authDomain: 'bcedaccess-website.firebaseapp.com',
+    databaseURL: 'https://bcedaccess-website.firebaseio.com',
+    projectId: 'bcedaccess-website',
+    storageBucket: 'bcedaccess-website.appspot.com',
+    messagingSenderId: '595114581930',
+    appId: '1:595114581930:web:39f9f01d8e2f66ef27d5d3',
+    measurementId: 'G-DHBS9MDXJM'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -26,7 +42,9 @@ import { FooterComponent } from './components/footer/footer.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   exports: [
     HeaderComponent,
