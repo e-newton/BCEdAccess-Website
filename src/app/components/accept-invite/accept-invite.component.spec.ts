82cde461cc16d5f750560384c29b8597d7abe331
e-newton <ericnewton2251@gmail.com>
2020-12-06 20:40:06 -0800

    All tests pass

--- src/app/components/accept-invite/accept-invite.component.spec.ts
+++ src/app/components/accept-invite/accept-invite.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AcceptInviteComponent } from './accept-invite.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ActivatedRoute, Router, RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {routes} from '../../app-routing.module';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../app.module';
+import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
 
 describe('AcceptInviteComponent', () => {
   let component: AcceptInviteComponent;
@@ -8,6 +16,13 @@ describe('AcceptInviteComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,
+        RouterModule.forRoot([]),
+        FormsModule,
+        RouterTestingModule.withRoutes(routes),
+        ReactiveFormsModule, FormsModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        CKEditorModule],
       declarations: [ AcceptInviteComponent ]
     })
     .compileComponents();
@@ -16,6 +31,10 @@ describe('AcceptInviteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AcceptInviteComponent);
     component = fixture.componentInstance;
+    let router;
+    router = TestBed.inject(Router);
+    let route;
+    route = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
