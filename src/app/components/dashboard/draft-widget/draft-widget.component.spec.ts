2c03626e259bed9186a2e95c2955e642650620c7
e-newton <ericnewton2251@gmail.com>
2020-12-28 17:03:41 -0800

    draft-widget.component.spec.ts completed

--- src/app/components/dashboard/draft-widget/draft-widget.component.spec.ts
+++ src/app/components/dashboard/draft-widget/draft-widget.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { DraftWidgetComponent } from './draft-widget.component';
 import {BlogService} from '../../../services/blog.service';
@@ -11,6 +11,7 @@ import {AngularFireModule} from '@angular/fire';
 import {environment} from '../../../app.module';
 import {CKEditorComponent, CKEditorModule} from '@ckeditor/ckeditor5-angular';
 import {BlogEditorComponent} from '../../blog-editor/blog-editor.component';
+import {Blog} from '../../../model/blog';
 
 describe('DraftWidgetComponent', () => {
   let component: DraftWidgetComponent;
@@ -19,6 +20,8 @@ describe('DraftWidgetComponent', () => {
   let activatedRoute: ActivatedRoute;
   let route: ActivatedRoute;
   let router: Router;
+  const b1 = new Blog(1, 'Title', 'Author', 'Body', 42, true, new Date(), false);
+  const b2 = new Blog(2, 'Title', 'Author', 'Body', 42, true, new Date(), false);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -48,4 +51,35 @@ describe('DraftWidgetComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load with all draft blogs', fakeAsync(() => {
+    const blogSpy = spyOn(blogService, 'getAllDraftBlogs').and.returnValues(Promise.resolve([b1, b2]));
+    component = new DraftWidgetComponent(blogService, router);
+    tick(100);
+    expect(blogSpy).toHaveBeenCalledTimes(1);
+    expect(component.blogs).toContain(b1);
+    expect(component.blogs).toContain(b2);
+  }));
+
+  it('should delete a blog', fakeAsync(async () => {
+    const delSpy = spyOn(blogService, 'deleteBlog').and.stub();
+    const draftSpy = spyOn(blogService, 'getAllDraftBlogs').and.returnValues(Promise.resolve([b2]));
+    component.blogs = [b1, b2];
+    await component.deleteBlog(b1.id);
+    expect(delSpy).toHaveBeenCalledWith(String(b1.id));
+    expect(delSpy).toHaveBeenCalledTimes(1);
+    expect(draftSpy).toHaveBeenCalledTimes(1);
+    expect(component.blogs).not.toContain(b1);
+  }));
+
+  it('should publish a blog', fakeAsync(async () => {
+    const pubSpy = spyOn(blogService, 'publishBlog').and.stub();
+    const draftSpy = spyOn(blogService, 'getAllDraftBlogs').and.returnValues(Promise.resolve([b2]));
+    component.blogs = [b1, b2];
+    await component.publishBlog(b1.id);
+    expect(pubSpy).toHaveBeenCalledWith(String(b1.id));
+    expect(pubSpy).toHaveBeenCalledTimes(1);
+    expect(draftSpy).toHaveBeenCalledTimes(1);
+    expect(component.blogs).not.toContain(b1);
+  }));
 });
