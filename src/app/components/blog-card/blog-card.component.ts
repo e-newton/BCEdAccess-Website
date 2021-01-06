6d050466d29ae97fb71ee93e3a39644562b3dee7
e-newton <ericnewton2251@gmail.com>
2021-01-06 15:44:26 -0800

    Small speed boost in cover image from local caching and RXJS Caching

--- src/app/components/blog-card/blog-card.component.ts
+++ src/app/components/blog-card/blog-card.component.ts
@@ -1,6 +1,7 @@
 import {AfterContentInit, AfterViewInit, ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {Reference} from '@angular/fire/storage/interfaces';
+import {FirebaseStorageImageCacheService} from '../../services/firebase-storage-image-cache.service';
 
 @Component({
   selector: 'app-blog-card',
@@ -11,27 +12,12 @@ export class BlogCardComponent implements OnInit {
   @Input() id: string;
   @Input() title: string;
   coverImageURL = 'assets/bcedaccess_logo.png';
-  constructor(public as: AngularFireStorage, private cdRef: ChangeDetectorRef) { }
+  constructor(public as: AngularFireStorage, private cdRef: ChangeDetectorRef, private fsic: FirebaseStorageImageCacheService) { }
 
   ngOnInit(): void {
-    this.as.ref(`blogs/${this.id}/`).listAll().toPromise().then((images) => {
-      let coverRef: Reference;
-      for (const ref of images.items) {
-        console.log(this.id, ref.name);
-        if (ref.name.includes('cover_image.')) {
-          coverRef = ref;
-          break;
-        }
-      }
-      if (!coverRef) {
-        return;
-      }
-      console.log('Running Update');
-      coverRef.getDownloadURL().then((url) => {
-        this.coverImageURL = url;
-        console.log('Cover Image URL', this.coverImageURL);
-        this.cdRef.detectChanges();
-      });
+    this.fsic.getBlogCoverImage(this.id).then((url) => {
+      this.coverImageURL = url;
+      this.cdRef.detectChanges();
     });
   }
 
--- src/app/services/firebase-storage-image-cache.service.ts
+++ src/app/services/firebase-storage-image-cache.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {ListResult, Reference} from '@angular/fire/storage/interfaces';
+import {shareReplay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseStorageImageCacheService {
+
+  coverImages = {};
 
   constructor(public as: AngularFireStorage) { }
 
@@ -24,6 +27,13 @@ export class FirebaseStorageImageCacheService {
   }
 
   async uploadBlogCoverImage(id: string|number, file: File): Promise<void> {
+    const files = await this.as.ref(`blogs/${id}/`).listAll().toPromise();
+    for (const file1 of files.items) {
+      if (file1.name.includes('cover_image.')){
+        await file1.delete();
+        break;
+      }
+    }
     const ext = file.name.substr(file.name.lastIndexOf('.') + 1);
     const storageRef = await this.as.ref(`blogs/${id}/cover_image.${ext}`);
     await storageRef.put(file);
@@ -31,5 +41,30 @@ export class FirebaseStorageImageCacheService {
 
   async deleteBlogImageCache(id: string|number): Promise<void> {
     await this.as.ref(`blog/${id}`).delete().toPromise();
+  }
+
+  async getBlogCoverImage(id: string|number): Promise<string> {
+    if (this.coverImages[id]){
+      return this.coverImages[id];
+    }
+    return await this.as.ref(`blogs/${id}/`).listAll().pipe(
+      shareReplay(1)
+    ).toPromise().then(async (images) => {
+      let coverRef: Reference;
+      for (const ref of images.items) {
+        console.log(id, ref.name);
+        if (ref.name.includes('cover_image.')) {
+          coverRef = ref;
+          break;
+        }
+      }
+      if (!coverRef) {
+        return 'assets/bcedaccess_logo.png';
+      }
+      console.log('Running Update');
+      const url = await coverRef.getDownloadURL();
+      this.coverImages[id] = url;
+      return url;
+    });
   }
 }
