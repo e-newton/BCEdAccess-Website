d76692bcf3f764fa808d849dcc82aa360ffe6c17
e-newton <ericnewton2251@gmail.com>
2021-01-14 13:31:02 -0800

    Collapse Cards finished

--- src/app/components/grape-editor/bootstrap-collapse.js
+++ src/app/components/grape-editor/bootstrap-collapse.js
@@ -1,7 +1,7 @@
 import grapesjs from 'grapesjs';
 function generateID(length = 6) {
   let result           = '';
-  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   const charactersLength = characters.length;
   for ( var i = 0; i < length; i++ ) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -15,30 +15,28 @@ export default grapesjs.plugins.add('bootstrap-collapse', (editor, options) => {
   const defaultModel = defaultType.model;
   const defaultView = defaultType.view;
 
-  domComps.addType('collapse-card', {
+  const accordionComp = domComps.addType('collapse-card', {
     model: defaultModel.extend({
       init(){
-        console.log('YA BITCH')
         const id = generateID();
         const header_id = generateID()
         const parent = this.parent();
-        console.log('PARENT', parent);
         this.components().add(`<div class="card">
-                                  <div class="card-header" id="${header_id}">
+                                  <div class="card-header" id="${header_id}" data-gjs-custom-name="Collapse Card Header">
                                     <h2 class="mb-0">
-                                      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${id}" aria-expanded="true" aria-controls="${id}">
-                                        Collapsible Group Item #1
+                                      <button class="btn btn-link" data-gjs-custom-name="Collapse Card Button" type="button" data-toggle="collapse" data-target="#${id}" aria-expanded="true" aria-controls="${id}">
+                                        Collapsible Group Item
                                       </button>
                                     </h2>
                                   </div>
 
-                                  <div id="${id}" class="collapse show" aria-labelledby="${header_id}" data-parent="#accordionExample">
+                                  <div id="${id}" class="collapse show" aria-labelledby="${header_id}" data-parent="#${parent.ccid}" data-gjs-custom-name="Collapse Card Body">
                                     <div class="card-body">
                                       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                                     </div>
                                   </div>
                                 </div>`)
-      }
+      },
     }, {
       isComponent(el) {
         if (el.tagName === 'COLLAPSE-CARD') {
@@ -48,18 +46,18 @@ export default grapesjs.plugins.add('bootstrap-collapse', (editor, options) => {
     }),
     view: defaultView
   })
-
+  //domComps.addComponent('Accordion').set({type: 'collapse-card'}).addTrait({type: 'checkbox', name: 'open'});
   editor.BlockManager.add('Accordion', {
-    label: "Accordion",
+    label: "Bootstrap Components",
     category: 'fuc',
-    content: '<div class="accordion" id="accordionExample">' +
+    content: '<div class="accordion" data-gjs-custom-name="Accordion">' +
       '<collapse-card></collapse-card>' +
       '<collapse-card></collapse-card>' +
       '<collapse-card></collapse-card>'+
       '</div>'
   })
   editor.BlockManager.add('Accordion-Card', {
-    label: "Accordion Card",
+    label: "Bootstrap Components",
     category: 'fuc',
     droppable: '.accordion',
     draggable: '.accordion',
