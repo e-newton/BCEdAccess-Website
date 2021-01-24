e19f478b5476ccc4b65e3b7deebfc093b6077bc3
e-newton <ericnewton2251@gmail.com>
2021-01-24 15:46:17 -0800

    Initialize the page-tree.component.ts

---
+++ src/app/components/page-tree/page-tree.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageTreeComponent } from './page-tree.component';
+
+describe('PageTreeComponent', () => {
+  let component: PageTreeComponent;
+  let fixture: ComponentFixture<PageTreeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageTreeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageTreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/page-tree/page-tree.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-tree',
+  templateUrl: './page-tree.component.html',
+  styleUrls: ['./page-tree.component.css']
+})
+export class PageTreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
