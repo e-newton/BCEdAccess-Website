396a022bd5bb579a7600246d26ca58050fb5c9ef
e-newton <ericnewton2251@gmail.com>
2021-02-15 22:29:11 -0800

    Fixed test

--- src/app/model/page.spec.ts
+++ src/app/model/page.spec.ts
@@ -2,6 +2,6 @@ import { Page } from './page';
 
 describe('Page', () => {
   it('should create an instance', () => {
-    expect(new Page('parent', 'title', 'body', true, 'id')).toBeTruthy();
+    expect(new Page('parent', 'title', 'body', true, 'id', false)).toBeTruthy();
   });
 });
