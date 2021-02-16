1dc3fab5c0410cdb8c64dbe138a71f41b6b75612
e-newton <ericnewton2251@gmail.com>
2021-02-15 22:37:01 -0800

    All tests passing (just missing imports)

--- src/app/components/grape-editor/grape-editor.component.spec.ts
+++ src/app/components/grape-editor/grape-editor.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GrapeEditorComponent } from './grape-editor.component';
+import {ActivatedRoute, RouterModule} from '@angular/router';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../app.module';
+import {PageService} from '../../services/page.service';
 
 describe('GrapeEditorComponent', () => {
   let component: GrapeEditorComponent;
@@ -8,6 +12,8 @@ describe('GrapeEditorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule.forRoot([]),
+        AngularFireModule.initializeApp(environment.firebase)],
       declarations: [ GrapeEditorComponent ]
     })
     .compileComponents();
@@ -15,6 +21,8 @@ describe('GrapeEditorComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GrapeEditorComponent);
+    TestBed.inject(ActivatedRoute);
+    TestBed.inject(PageService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/components/page-tree/page-tree.component.spec.ts
+++ src/app/components/page-tree/page-tree.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageTreeComponent } from './page-tree.component';
+import {ActivatedRoute, RouterModule} from '@angular/router';
+import {PageService} from '../../services/page.service';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../app.module';
 
 describe('PageTreeComponent', () => {
   let component: PageTreeComponent;
@@ -8,6 +12,8 @@ describe('PageTreeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule.forRoot([]),
+        AngularFireModule.initializeApp(environment.firebase)],
       declarations: [ PageTreeComponent ]
     })
     .compileComponents();
@@ -15,6 +21,8 @@ describe('PageTreeComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PageTreeComponent);
+    TestBed.inject(ActivatedRoute);
+    TestBed.inject(PageService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
