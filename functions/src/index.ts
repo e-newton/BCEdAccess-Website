2c728d07a6f16c6ee8df3d698074e0fe5cd8f4f2
e-newton <ericnewton2251@gmail.com>
2021-02-17 16:34:55 -0800

    SEO with local rendertron now working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,14 @@ import * as mailer from 'nodemailer';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
+import * as url from 'url';
+import fetch from 'node-fetch';
+// const fetch = require('node-fetch');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 const viewDecrementValue = 10;
 const app = express();
+
 app.use(cors());
 admin.initializeApp();
 
@@ -18,6 +22,9 @@ const mailTransport = mailer.createTransport({
     pass: functions.config().email.pass,
   },
 });
+
+const appURL = 'bcedaccess-website.web.app';
+const renderURL = 'https://rendertron-305123.wl.r.appspot.com/render';
 
 app.post('/blogs/', async (req, res) => {
   functions.logger.info('Blog View', req.body.id);
@@ -34,7 +41,66 @@ app.post('/pagefiles/',  async (req, res) => {
   functions.logger.info('Page File', req.body);
 });
 
-export const httpRequests = functions.https.onRequest(app);
+function generateUrl(request: any): string {
+  return url.format({
+    protocol: 'https:',
+    host: appURL,
+    pathname: request.originalUrl,
+  });
+}
+
+function detectBot(userAgent: any): boolean {
+  const bots = [
+    'googlebot',
+    'bingbot',
+    'yandexbot',
+    'duckduckbot',
+    'slurp',
+
+    'twitterbot',
+    'facebookexternalhit',
+    'linkedinbot',
+    'embedly',
+    'pinterest',
+    'slackbot',
+    'facebot',
+
+  ];
+
+  const agent = userAgent.toLowerCase();
+  for (const bot of bots) {
+    if (agent.indexOf(bot) > -1) {
+      console.log('BOT', bot);
+      return true;
+    }
+  }
+  return false;
+}
+
+app.get('*', (req, res) => {
+  const isBot = detectBot(req.headers['user-agent']);
+
+  if (isBot) {
+    const botURL = generateUrl(req);
+    functions.logger.info('url being fetched', `${renderURL}/${botURL}`);
+    fetch(`${renderURL}/${botURL}`)
+      .then((r: any) => r.text())
+      .then((body: any) => {
+        res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+        res.set('Vary', 'User-Agent');
+        res.send(body.toString());
+      });
+  } else{
+    fetch(`https://${appURL}`)
+      .then((r: any) => r.text())
+      .then((body: any) => {
+        res.send(body.toString());
+      });
+  }
+
+});
+
+exports.app = functions.https.onRequest(app);
 
 
 export const decrementBlogViews = functions.pubsub.schedule('30 5 * * *').onRun((context) => {
