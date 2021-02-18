ca77b8330e72f9727951b591b1ce60098497784d
e-newton <ericnewton2251@gmail.com>
2021-02-17 17:04:52 -0800

    Added test to SEO Service

--- src/app/services/seo.service.spec.ts
+++ src/app/services/seo.service.spec.ts
@@ -1,16 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
+import {Meta} from '@angular/platform-browser';
 
-import { SeoService } from './seo.service';
+
+import {SeoService} from './seo.service';
 
 describe('SeoService', () => {
   let service: SeoService;
+  let meta: Meta;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SeoService);
+    meta = TestBed.inject(Meta);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should generate default tags with no config given', fakeAsync(() => {
+    const htmlSpy = spyOn(meta, 'updateTag').and.stub();
+    const genSpy = spyOn(service, 'generateTags').and.stub();
+    service.generateTags({});
+    expect(genSpy).toHaveBeenCalledWith({});
+    expect(htmlSpy).toHaveBeenCalledWith({ name: 'twitter:card', content: 'summary' });
+  }));
 });
